46f4475646fa0d45ea9a12483d60c9a7133f590f
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-08 20:40:04 -0300

    Desarrollo de ejercicio 1 y 2.
    alert, prompt, variable, valores literales, undefined.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,17 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+
+	var nombre; //defino una variable
+	nombre = prompt("ingrese su nombre","natalia natalia");
+	alert(nombre);
+	/*
+	nombre = "Yesid Dario Guinard Silva";
+	prompt("Please enter your name", "Harry Potter");
+	alert(nombre);		//variable		
+	alert("nombre");	 //palabra literal!
+	*/
+	//var person = prompt("Please enter your name", "Harry Potter");
 	
 }
 
