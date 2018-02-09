032db151a40b560d1333bd7e9a006f9c770d1bd5
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-09 20:31:58 -0300

    Parcial 1
    Calcular Perimetro Cuadrado. Input ID y output by alert

--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,12 @@
 
 function Mostrar()
 {
+	var baseCuadrado;
+	var perimetroCuadrado;
+	
+	baseCuadrado = document.getElementById('laBase').value;
+	baseCuadrado = parseInt(baseCuadrado);
+	perimetroCuadrado = 4 * baseCuadrado ;
 
+	alert("El perimetro del cuadrado es: " + perimetroCuadrado);
 }
