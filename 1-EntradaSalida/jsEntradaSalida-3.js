f36121c09264abbe6f401f96fa51fbda443e716e
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-08 21:35:37 -0300

    Ejercicios 3, 4 y 5.
    concepto  edad = document.getElementById('laEdad').value; // Input.
    document.getElementById('elNombre').value = nombre;  // Output ID.

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,10 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+	nombre = document.getElementById('elNombre').value; // id se encuentra definido en el html
+	alert(nombre);
+
 }
 
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,8 @@
 */
 function Mostar()
 {
-	
+	var nombre;
+	nombre = prompt("Ingresa tu nombre");
+	document.getElementById('elNombre').value = nombre;
 }
 
