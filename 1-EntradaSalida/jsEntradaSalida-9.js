fae61bd89f3318f97488ffc1032d442c663d0cf5
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-09 19:46:03 -0300

    Ejercicio 9
    concepto de porcentaje, getElementById como Input y como Output

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var sueldoConAumento;
+	var porcentaje = 10;
 
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+	sueldoConAumento = sueldo + (sueldo*porcentaje/100);
+
+	
+	document.getElementById('resultado').value = sueldoConAumento; 
 	
 }
