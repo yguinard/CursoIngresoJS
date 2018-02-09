72b8461c02ed001de5023a362d9a96eade66a9c3
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-09 20:58:54 -0300

    ejercicio 2 Parcial 1

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,13 @@
+
 function Mostrar()
 {
+  var valorfinal;
+  var porcentajeIva=21;
+  var valorInicial;
+
+  valorInicial = prompt("Ingrese importe:","0");
+  valorInicial = parseInt(valorInicial);
+  valorfinal = valorInicial + (valorInicial*porcentajeIva/100);
   
+  document.getElementById('importeFinal').value = valorfinal;
 }
