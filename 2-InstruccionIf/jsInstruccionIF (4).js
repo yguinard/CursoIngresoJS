87e7e1a9aaebe7b6d34607a3f1007eb9d2e08f66
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-09 21:51:50 -0300

    ejercicio 3 y 4

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,11 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad = parseInt(document.getElementById('edad').value);
 
-
+	if((edad>=13)&&(edad<=17)){
+		alert("Es adolecente");
+	}
 
 }//FIN DE LA FUNCIÓN
