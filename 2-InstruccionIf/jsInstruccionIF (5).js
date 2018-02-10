f413573959301cec84c54c6372c1347aec804b9b
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-09 22:00:53 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad = parseInt(document.getElementById('edad').value);
 
+	if((edad<13)||(edad>17)){
+
+		alert("No es adolescente");	
+	}
+
+	
 
 }//FIN DE LA FUNCIÓN
