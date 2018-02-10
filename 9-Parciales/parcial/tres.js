9796fc69dc232da2d99cd0317b2a6673889551ef
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-02-09 21:06:47 -0300

    ejercicio 3 parcial1

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,18 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetroTerreno;
+	var numeroDeLineas = 3; 
+
+	largo = document.getElementById('alrgo').value;
+	ancho = document.getElementById('ancho').value;
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	perimetroTerreno = (largo *2) + (ancho * 2);
+	perimetroTerreno = numeroDeLineas * perimetroTerreno;
+	alert("Metros de alambre Necesarios: " + perimetroTerreno);
 
 }
