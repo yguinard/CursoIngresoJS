d4bca15314353b42b66d32857426440def75aa30
Yesid Guinard <32947989+yguinard@users.noreply.github.com>
2018-03-02 22:18:39 -0300

    6 y 7

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,48 @@
+/*
+Enunciado:
+Bienvenidos. 
+Realizar el algoritmo que pida el importe de las ventas (validar que sea mayor a 0,”cero”)
+ de los 24 días hábiles del mes por prompt (una por día), e informar cual fue el mayor importe y 
+ cuál fue el menor importe de venta.
+*/
 function Mostrar()
 {
+	var importeIngresado;
+	var importeMaximo;
+	var importeMinimo;
+	var contador=0;
+	
+	while(contador<25)
+	{
+		contador++;
+		importeIngresado=prompt("Ingrese Importe #"+contador);
+		importeIngresado=parseInt(importeIngresado);
 
+		while(isNaN(importeIngresado)||importeIngresado<=0)
+		{
+
+			importeIngresado=prompt("Ingrese Importe #"+contador);
+			importeIngresado=parseInt(importeIngresado);
+
+		}
+
+		if(contador==1)
+		{
+			importeMaximo=importeIngresado;
+			importeMinimo=importeIngresado;
+		}
+
+		if(importeIngresado>importeMaximo)
+		{	
+			importeMaximo=importeIngresado;
+		}
+
+		if(importeIngresado<importeIngresado)
+		{
+			importeMinimo=importeIngresado;
+		}
+	}	
+
+	document.write("<br>Importe minimo ingresado: " + importeMinimo);
+	document.write("<br>Importe maximo ingresado: " + importeMaximo);
 }
--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,70 @@
+/*
+Enunciado:
+Bienvenidos. 
+Realizar el algoritmo que permita el ingreso por prompt de las notas 
+(validar entre 0 y 10) y el sexo (validar el sexo “f” o “m”) de 100 alumnos, 
+informar por alert: a) el promedio de las notas totales. b) la nota más baja. c) 
+la cantidad de varones que su nota haya sido mayor o igual a 6.
+*/
 function Mostrar()
 {
 
+	var notaDigitada;
+	var sexoDigitado;
+	var promedioNotas;
+	var acumuladorNotas=0;
+	var cantidadVaronesNotaMayor=0;
+	var notaMasBaja;
+	var NotaMasAlta;
+	var cantidadDeAlumnos=0;
+
+	while(cantidadDeAlumnos<10)
+	{
+		cantidadDeAlumnos++;
+
+		notaDigitada = prompt("Ingrese Nota del Alumno #" + cantidadDeAlumnos);
+		notaDigitada = parseInt(notaDigitada);
+
+		while(isNaN(notaDigitada)|| notaDigitada<0|| notaDigitada>10)
+		{
+			notaDigitada = prompt("Error!Ingrese Nota del Alumno #" + cantidadDeAlumnos);
+			notaDigitada = parseInt(notaDigitada);
+		}
+
+		sexoDigitado = prompt("Digite el Sexo del Alumno f o m");
+
+		while(sexoDigitado!='f' && sexoDigitado!='m')
+		{
+			sexoDigitado = prompt("Error!. Digite el Sexo del Alumno f o m");
+		}
+
+		acumuladorNotas = acumuladorNotas + notaDigitada;
+		
+		if(sexoDigitado=='m' && notaDigitada>=6)
+		{
+			cantidadVaronesNotaMayor++;
+		}
+
+		if(cantidadDeAlumnos==1)
+		{
+			notaMasBaja = notaDigitada;
+			NotaMasAlta = notaDigitada;
+		}
+
+		if(notaDigitada<notaMasBaja)
+		{
+			notaMasBaja=notaDigitada;
+		}
+
+		if(notaDigitada>NotaMasAlta)
+		{
+			NotaMasAlta=notaDigitada;
+		}
+
+	}
+
+
+	promedioNotas=acumuladorNotas/10;
+
+alert("Promedio de notas Totales: " +promedioNotas+ "  Nota mas Baja: " + notaMasBaja + "  Nota mas Alta: "+ NotaMasAlta + " Ctd de Varones> 6: " +cantidadVaronesNotaMayor);
 }
